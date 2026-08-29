008a0efe1178655117d902cef1beedde6f7bd578
Kratik Mudgal <43618810+moonboyknm@users.noreply.github.com>
2026-08-30 00:04:51 +0530

    Add: binary-search - submission-3

---
+++ Data Structures & Algorithms/binary-search/submission-3.cpp
@@ -0,0 +1,32 @@
+/*
+a --> array / nums
+x --> target / marks the spot!
+l --> left
+r --> right
+
+when mid is bigger than x, move right just behind mid. 
+when mid is bigger than x, move left just after mid.
+
+That's the secret.
+*/
+
+class Solution {
+public:
+    int search(vector<int>& a, int x) {
+        int l = 0;
+        int r = a.size() - 1;
+
+        while (l <= r){
+            int mid = l + ((r - l) / 2);
+
+            if (a[mid] > x){
+                r = mid - 1;
+            } else if (a[mid] < x){
+                l = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+};
