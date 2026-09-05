3ce09de5f97827e6aa014890f3a5a5a36f2019e2
Kratik Mudgal <43618810+moonboyknm@users.noreply.github.com>
2026-09-05 17:23:21 +0530

    Add: time-based-key-value-store - submission-3

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-3.cpp
@@ -0,0 +1,28 @@
+class TimeMap {
+private:
+    unordered_map<string, vector<pair<int, string>>> keyStore;
+
+public:
+    void set(string key, string value, int timestamp) {
+        keyStore[key].emplace_back(timestamp, value);
+    }
+    
+    string get(string key, int timestamp) {
+        auto& values = keyStore[key];
+        int l = 0, r = values.size() - 1;
+        string res = "";
+
+        while(l <= r){
+            int mid = l + (r - l) / 2;
+
+            if (timestamp >= values[mid].first){
+                res = values[mid].second;
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+
+        return res;
+    }
+};
